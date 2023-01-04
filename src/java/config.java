18da5e8da647fb1bad245e164b355ad0b0461854
rvz <rviz@pm.me>
2023-01-04 15:42:42 +0530

    updated login servelt

--- src/java/config.java
+++ src/java/config.java
@@ -16,17 +16,12 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = {"/config"})
 public class config extends HttpServlet {
-
-   
-    
-    
   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
     }
-
    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -35,77 +30,68 @@ public class config extends HttpServlet {
         try
         {
 
-        String username=request.getParameter("username");
+        String email=request.getParameter("email");
         String password=request.getParameter("password");
         
+        String HashedPassword = null;             
+        // MessageDigest instance for MD5.  
+        MessageDigest m = MessageDigest.getInstance("MD5");                
+        // Add plain-text password bytes to digest using MD5 update() method. 
+        m.update(password.getBytes());      
+        // Convert the hash value into bytes
+        byte[] bytes = m.digest();  
+        // The bytes array has bytes in decimal form. Converting it into hexadecimal format.  
+        StringBuilder s = new StringBuilder();  
+            for(int i=0; i< bytes.length ;i++)  
+            {  
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));  
+            }                
+        // Complete hashed password in hexadecimal format  
+        HashedPassword = s.toString();  
         
-        PrintWriter out=response.getWriter();
         
-        Connection con=null;
-        
+        // db configurations
+        PrintWriter out=response.getWriter();        
+        Connection con=null;        
         Statement st=null;
         
           Class.forName("com.mysql.jdbc.Driver");
-          con=(Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/PUSL2024-Project","root","");
+          con=(Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/PUSL2024-Project/customer","root","");
           st=con.createStatement();
             
-          PreparedStatement ps=con.prepareStatement("SELECT username FROM details WHERE username=? AND password=?");
-          ps.setString(1, username);
-          ps.setString(2, password);
+          PreparedStatement ps=con.prepareStatement("SELECT email FROM customer WHERE email=? AND password=?");
+          ps.setString(1, email);
+          ps.setString(2, HashedPassword);
             
-            ResultSet rs=ps.executeQuery();
+          ResultSet rs=ps.executeQuery();
             
             
             if(rs.next())
-                {
-                
-                    // If the username and password are valid, log the user in
+                {                
+                    // If the email and password are valid, log the user in
                     HttpSession session = request.getSession();
-                    session.setAttribute("username", username);
+                    session.setAttribute("email", email);
                     session.setMaxInactiveInterval(10*60); // 10 minutes
                     
-                    Cookie loginCookie = new Cookie("username", username);
+                    Cookie loginCookie = new Cookie("email", email);
                     loginCookie.setPath("/");
                     response.addCookie(loginCookie);
     
-
                     // Redirect the user to the homepage
-                    response.sendRedirect("movies.jsp");
-
-                    
+                    response.sendRedirect("movies.jsp");                    
                 
                }
             
             else
                 {
                    
-                    String message = "Invalid username or password!";
+                    String message = "Invalid email or password!";
                     request.setAttribute("message", message);
                     request.getRequestDispatcher("/index.jsp").forward(request, response);
                 }
             
             
-            String encryptedpassword = null; 
-            
-            // MessageDigest instance for MD5.  
-            MessageDigest m = MessageDigest.getInstance("MD5");  
-              
-            // Add plain-text password bytes to digest using MD5 update() method. 
-            m.update(password.getBytes());  
-              
-            // Convert the hash value into bytes
-            byte[] bytes = m.digest();  
-              
-            // The bytes array has bytes in decimal form. Converting it into hexadecimal format.  
-            StringBuilder s = new StringBuilder();  
-            for(int i=0; i< bytes.length ;i++)  
-            {  
-                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));  
-            }  
-              
-            // Complete hashed password in hexadecimal format  
-            encryptedpassword = s.toString();  
-            
+ 
             
             
             
