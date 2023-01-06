3e8608babadefd8185f3df3a5f460603b0041023
Sanidu Anupama <92055798+saniduanupama@users.noreply.github.com>
2023-01-06 19:07:48 +0530

    sql file added

---
+++ abc_cinema.sql
@@ -0,0 +1,355 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 06, 2023 at 02:36 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `abc_cinema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adult_tickets`
+--
+
+CREATE TABLE `adult_tickets` (
+  `t_id` int(11) NOT NULL,
+  `a_price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `child_tickets`
+--
+
+CREATE TABLE `child_tickets` (
+  `t_id` int(11) NOT NULL,
+  `c_price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `c_id` int(11) NOT NULL,
+  `c_fname` varchar(256) NOT NULL,
+  `c_lname` varchar(256) NOT NULL,
+  `c_mobile` varchar(10) NOT NULL,
+  `c_email` varchar(256) NOT NULL,
+  `c_password` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`c_id`, `c_fname`, `c_lname`, `c_mobile`, `c_email`, `c_password`) VALUES
+(2001, 'Jagath', 'Perera', '0700987654', 'jagathperera@gmail.com', '1e35e5408e33e27464b3f6c3bff66a76'),
+(2004, 'Saman', 'Pieris', '0750472729', 'samanpeiris@gmail.com', '7eb681a31a3c08ee6a429a81fc97e677'),
+(2005, 'Sujeewa', 'Sampath', '0717683945', 'sujeewasampath@gmail.com', 'af4054d5bdcd30334fc94444a8888dcd'),
+(2006, 'James', 'Peterson', '0763727927', 'jamespeterson@gmail.com', 'b4cc344d25a2efe540adbf2678e2304c');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `emp_id` int(11) NOT NULL,
+  `e_name` varchar(256) NOT NULL,
+  `e_email` varchar(256) NOT NULL,
+  `e_password` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager`
+--
+
+CREATE TABLE `manager` (
+  `emp_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie`
+--
+
+CREATE TABLE `movie` (
+  `m_id` int(11) NOT NULL,
+  `m_name` varchar(256) NOT NULL,
+  `m_language` varchar(256) NOT NULL,
+  `runtime` time NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `m_status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie_genre`
+--
+
+CREATE TABLE `movie_genre` (
+  `m_id` int(11) NOT NULL,
+  `genre` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie_show`
+--
+
+CREATE TABLE `movie_show` (
+  `show_id` int(11) NOT NULL,
+  `show_date_time` datetime NOT NULL,
+  `m_id` int(11) NOT NULL,
+  `emp_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `pay_id` int(11) NOT NULL,
+  `total_amount` int(11) DEFAULT NULL,
+  `c_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seats`
+--
+
+CREATE TABLE `seats` (
+  `seat_no` varchar(2) NOT NULL,
+  `seat_status` tinyint(1) NOT NULL,
+  `show_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `emp_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `t_id` int(11) NOT NULL,
+  `seat_no` varchar(2) DEFAULT NULL,
+  `show_id` int(11) DEFAULT NULL,
+  `c_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adult_tickets`
+--
+ALTER TABLE `adult_tickets`
+  ADD PRIMARY KEY (`t_id`);
+
+--
+-- Indexes for table `child_tickets`
+--
+ALTER TABLE `child_tickets`
+  ADD PRIMARY KEY (`t_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- Indexes for table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- Indexes for table `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`m_id`);
+
+--
+-- Indexes for table `movie_genre`
+--
+ALTER TABLE `movie_genre`
+  ADD PRIMARY KEY (`m_id`);
+
+--
+-- Indexes for table `movie_show`
+--
+ALTER TABLE `movie_show`
+  ADD PRIMARY KEY (`show_id`),
+  ADD KEY `m_id` (`m_id`),
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`pay_id`),
+  ADD KEY `c_id` (`c_id`);
+
+--
+-- Indexes for table `seats`
+--
+ALTER TABLE `seats`
+  ADD PRIMARY KEY (`seat_no`),
+  ADD KEY `show_id` (`show_id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`t_id`),
+  ADD KEY `seat_no` (`seat_no`),
+  ADD KEY `show_id` (`show_id`),
+  ADD KEY `c_id` (`c_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2007;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+
+--
+-- AUTO_INCREMENT for table `movie`
+--
+ALTER TABLE `movie`
+  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `movie_show`
+--
+ALTER TABLE `movie_show`
+  MODIFY `show_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `adult_tickets`
+--
+ALTER TABLE `adult_tickets`
+  ADD CONSTRAINT `adult_tickets_ibfk_1` FOREIGN KEY (`t_id`) REFERENCES `tickets` (`t_id`);
+
+--
+-- Constraints for table `child_tickets`
+--
+ALTER TABLE `child_tickets`
+  ADD CONSTRAINT `child_tickets_ibfk_1` FOREIGN KEY (`t_id`) REFERENCES `tickets` (`t_id`);
+
+--
+-- Constraints for table `manager`
+--
+ALTER TABLE `manager`
+  ADD CONSTRAINT `manager_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`);
+
+--
+-- Constraints for table `movie_genre`
+--
+ALTER TABLE `movie_genre`
+  ADD CONSTRAINT `movie_genre_ibfk_1` FOREIGN KEY (`m_id`) REFERENCES `movie` (`m_id`);
+
+--
+-- Constraints for table `movie_show`
+--
+ALTER TABLE `movie_show`
+  ADD CONSTRAINT `movie_show_ibfk_1` FOREIGN KEY (`m_id`) REFERENCES `movie` (`m_id`),
+  ADD CONSTRAINT `movie_show_ibfk_2` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`);
+
+--
+-- Constraints for table `seats`
+--
+ALTER TABLE `seats`
+  ADD CONSTRAINT `seats_ibfk_1` FOREIGN KEY (`show_id`) REFERENCES `movie_show` (`show_id`);
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`);
+
+--
+-- Constraints for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`seat_no`) REFERENCES `seats` (`seat_no`),
+  ADD CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`show_id`) REFERENCES `movie_show` (`show_id`),
+  ADD CONSTRAINT `tickets_ibfk_3` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
